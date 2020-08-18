80c4b7634c69669f92278c1c9e154da95ed044c4
Adit Modi <adit.modi24@gmail.com>
2020-08-18 11:24:53 +0530

    Update hivetable.sql

--- hivetable.sql
+++ hivetable.sql
@@ -22,4 +22,4 @@ WITH SERDEPROPERTIES (
 ) LOCATION '${INPUT}/cloudfront/data';
 
 -- Total requests per operating system for a given time frame
-INSERT OVERWRITE DIRECTORY '${OUTPUT}/os_requests/' SELECT os, COUNT(*) count FROM cloudfront_logs WHERE dateobject BETWEEN '2014-07-05' AND '2014-08-05' GROUP BY os;
+INSERT OVERWRITE DIRECTORY '${OUTPUT}/os_requests/' SELECT os, COUNT(*) count FROM cloudfront_logs WHERE dateobject BETWEEN '2014-07-05' AND '2014-08-05' GROUP BY os;asasasas
